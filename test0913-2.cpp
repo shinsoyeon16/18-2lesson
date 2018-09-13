9432b5311a0840fd2ceb148a18b7ab7aed3fbf3b
shinsoyeon <shinsoyeon16@yi.ac.kr>
2018-09-13 14:26:50 +0900

    0913-4

--- test0913-2.cpp
+++ test0913-2.cpp
@@ -6,11 +6,11 @@ void Set(char* s);
 int main() {
 	int a = 0;
 	char * s = (char*)&a;
-	//Set(s);
+	Set(s);
 	cout << (char*)&a << endl;
 }
 void Set(char* s) {
 	*s = 'C';
 	*(s + 1) = '+';
-	*(s + 2) = '+';
+	*(s + 2) = *(s + 1);
 }
